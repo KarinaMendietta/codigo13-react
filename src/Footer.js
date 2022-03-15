4cc720afcd9e845ff8c175e51e229907684b2619
KarinaMendietta <karinamendiettasoto@gmail.com>
2022-03-14 23:00:26 -0500

    react dia 1

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div>
+            <p>Trabaja con nosotros</p>
+            <p>Contactanos al 9999999</p>
+        </div>
+
+    );
+};
+
+export default Footer;
